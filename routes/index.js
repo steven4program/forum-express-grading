df4a68b14401cac7cb7daf1c14de89a968a724d3
steven4program <stevenchang421@gmail.com>
2021-11-14 22:02:01 +0800

    Add admin restaurant delete feature

--- routes/index.js
+++ routes/index.js
@@ -69,4 +69,9 @@ module.exports = (app, passport) => {
     authenticatedAdmin,
     adminController.putRestaurant
   )
+  app.delete(
+    '/admin/restaurants/:id',
+    authenticatedAdmin,
+    adminController.deleteRestaurant
+  )
 }
