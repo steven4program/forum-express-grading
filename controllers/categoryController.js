301cf7f83fd17baa32adcab8b3d59bab0d6c823d
steven4program <stevenchang421@gmail.com>
2021-11-16 22:42:13 +0800

    Add admin categories post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,6 +8,18 @@ const categoryController = {
     }).then((categories) => {
       return res.render('admin/categories', { categories })
     })
+  },
+  postCategory: (req, res) => {
+    const { name } = req.body
+    if (!name) {
+      req.flash('error_message', "name didn't exist")
+    } else {
+      return Category.create({
+        name
+      }).then((category) => {
+        res.redirect('/admin/categories')
+      })
+    }
   }
 }
 module.exports = categoryController
--- routes/index.js
+++ routes/index.js
@@ -94,4 +94,9 @@ module.exports = (app, passport) => {
     authenticatedAdmin,
     categoryController.getCategories
   )
+  app.post(
+    '/admin/categories',
+    authenticatedAdmin,
+    categoryController.postCategory
+  )
 }
