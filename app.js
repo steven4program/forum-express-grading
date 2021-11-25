7db555218cab89475b94ad4ee87ac059c7d9caa2
steven4program <stevenchang421@gmail.com>
2021-11-25 14:53:44 +0800

    refactor router & GET /api/admin/restaurants

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app, passport)
+require('./routes')(app)
 
 module.exports = app
